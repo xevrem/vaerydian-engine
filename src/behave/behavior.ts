e26cc6e31d32bf6384a2d78b61c649a45f852992
erika jonell <erika.jonell@gmail.com>
2020-07-09 07:37:20 -0400

    updates

--- src/behave/behavior.ts
+++ src/behave/behavior.ts
@@ -1,45 +1,18 @@
-export enum BehaviorReturnCode {
+export enum BehaviorCode {
   Failure,
   Running,
   Success,
 }
 
-
-export abstract class BehaviorComponent {
-  protected returnCode: BehaviorReturnCode;
-  public abstract behave(): BehaviorReturnCode;
+export abstract class Behavior {
+  protected returnCode: BehaviorCode;
+  public abstract behave(): BehaviorCode;
 }
 
-export class Behavior {
-  _root: BehaviorComponent;
-  _lastVisited: BehaviorComponent;
-  returnCode: BehaviorReturnCode;
-
-  constructor(root: BehaviorComponent) {
-    this._root = root;
-  }
-
-  behave(): BehaviorReturnCode {
-    try {
-      switch (this._root.behave()) {
-        case BehaviorReturnCode.Failure:
-          this.returnCode = BehaviorReturnCode.Failure;
-          break;
-        case BehaviorReturnCode.Success:
-          this.returnCode = BehaviorReturnCode.Success;
-          break;
-        case BehaviorReturnCode.Running:
-          this.returnCode = BehaviorReturnCode.Running;
-          break;
-        default:
-          this.returnCode = BehaviorReturnCode.Running;
-          break;
-      }
-      return this.returnCode;
-    } catch (error) {
-      console.error('b:b::error', error);
-      this.returnCode = BehaviorReturnCode.Failure;
-      return this.returnCode;
-    }
+export class Action extends Behavior {
+  behave: () => BehaviorCode;
+  constructor(action: () => BehaviorCode) {
+    super();
+    this.behave = action;
   }
 }
